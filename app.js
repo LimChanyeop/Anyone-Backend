2a9a452bf8229cf9530a2ac1685fae3c85b5face
chanyeop <153893@naver.com>
2023-02-15 22:28:14 +0900

    Feat: make info msg template

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require("express");
+var infoMsg = require("./message/msg_info");
 
 var app = express();
 
 // Default Setting
 const port = process.env.PORT || 3000;
+
+app.use("/", function (req, res, next) {
+	res.status(200).send(infoMsg.success);
+});
 
 app.use("/index", function (req, res, next) {
 	res.status(200).send(infoMsg.success);
